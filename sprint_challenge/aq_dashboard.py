7086fdd2200769f9e7ec297251f26eb2088e2dc1
Srini Nariangadu <srini.nariangadu@gmail.com>
2021-02-21 17:47:20 -0800

    Nearly complete

--- sprint_challenge/aq_dashboard.py
+++ sprint_challenge/aq_dashboard.py
@@ -1,8 +1,10 @@
 """OpenAQ Air Quality Dashboard with Flask."""
-from flask import Flask, render_template
+from os import urandom
+from flask import Flask, render_template, request
 from openaq import OpenAQ
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 CITY='Los Angeles'
@@ -10,17 +12,74 @@ COUNTRY='CL'
 LATEST_MEASUREMENT = None
 
 APP = Flask(__name__)
+APP.secret_key = urandom(42)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+APP.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+DB = SQLAlchemy(APP)
+MIGRATE = Migrate(APP, DB)
+
 
 API = OpenAQ()
 
 
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-DB = SQLAlchemy(APP)
+class City(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    city = DB.Column(DB.String, nullable=False)
+    country = DB.Column(DB.String)
+    latest_measurement = DB.Column(DB.String)
+
+class Record(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    datetime = DB.Column(DB.String(25))
+    value = DB.Column(DB.Float, nullable=False)
+
+    def __repr__(self):
+        return f'{self.datetime}:{self.value}'
+
+def get_city_from_db():
+    global CITY, COUNTRY, LATEST_MEASUREMENT
+
+    city = City.query.get(1)
+
+    if city:
+        COUNTRY=None
+        LATEST_MEASUREMENT=None
+
+        CITY = city.city
+        if city.country:
+            COUNTRY = city.country
+        if city.latest_measurement:
+            LATEST_MEASUREMENT = city.latest_measurement
+
+
+def update_city_in_db():
+    global CITY, COUNTRY, LATEST_MEASUREMENT
+
+    city = City.query.get(1)
+
+    if city:
+        DB.session.delete(city)
+        DB.session.commit()
+
+    params = {'id':1, 'city':CITY, 'country':COUNTRY}
+    if COUNTRY:
+        params['country'] = COUNTRY
+    if LATEST_MEASUREMENT:
+        params['latest_measurement'] = LATEST_MEASUREMENT
+    
+
+    city = City(**params)
+
+    DB.session.add(city)
+    DB.session.commit()
 
 def get_results(data='pm25'):
-    global LATEST_MEASUREMENT
+    global LATEST_MEASUREMENT, CITY, COUNTRY
 
-    params = {'city':CITY, 'country':COUNTRY, 'parameter':data}
+    params = {'city':CITY, 'parameter':data}
+    if COUNTRY:
+        params['COUNTRY'] = COUNTRY
     if LATEST_MEASUREMENT:
         params['date_from'] = LATEST_MEASUREMENT
 
@@ -30,37 +89,50 @@ def get_results(data='pm25'):
 
     if len(ret):
         LATEST_MEASUREMENT = ret[0][0]
+        update_city_in_db()
 
     return ret
-
-class Record(DB.Model):
-    id = DB.Column(DB.Integer, primary_key=True)
-    datetime = DB.Column(DB.DateTime)
-    value = DB.Column(DB.Float, nullable=False)
-
-    def __repr__(self):
-        return f'<{datetime}>:<{value}>'
 
 @APP.route('/')
 def root():
     """Base view."""
-    
-    danger_list = Record.query.filter(value >= 10).all()
+    get_city_from_db()
+
+    danger_list = Record.query.filter(Record.value >= 10).all()
     all_list = Record.query.all()
 
-    page_params = {'CITY':CITY, 'COUNTRY':COUNTRY, 'danger_list':danger_list, 'all_list':all_list}
+    page_params = {'CITY':CITY, 'COUNTRY':COUNTRY, 'danger_list':danger_list, 'data':'PM25', 'num_risky':len(danger_list), 'num_all': len(all_list)}
     return render_template('aq_layout.html', **page_params)
 
 
-@APP.route('/refresh')
+@APP.route('/refresh', methods=["POST"])
 def refresh():
     
+    get_city_from_db()
     data = get_results()
     for elem in data:
         rec = Record(datetime=elem[0], value=elem[1])
         DB.session.add(rec)
     DB.session.commit()
     return root()
+
+@APP.route('/change', methods=["POST"])
+def change():
+    global CITY, COUNTRY, LATEST_MEASUREMENT
+
+    form_data = request.form
+
+    CITY = form_data['city_name']
+    country_code = form_data['country_code']
+    COUNTRY= country_code if country_code else None
+    LATEST_MEASUREMENT = None
+
+    DB.drop_all()
+    DB.create_all()
+
+    update_city_in_db()
+
+    return refresh()
 
 if __name__ == "__main__":
     
